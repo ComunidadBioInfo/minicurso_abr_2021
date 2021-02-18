9b6d7becd39b897c73864741a81b5b6dd94e509d
necrosnake91 <rodolfo.peacewalker@gmail.com>
2021-02-18 11:05:54 -0600

    Update to script 3.1

--- bin/3.1.AbundanceEstimation.sh
+++ bin/3.1.AbundanceEstimation.sh
@@ -1,3 +1,3 @@
 ##This script performs index generation of the human transcriptome and gene pseudomapping using Samlmon
 ##Run salmon index to generate the index. 
-salmon index -t ../transcriptome/Homo_sapiens.GRCh38.cdna.all.fa.gz hsa_index
+~/salmon-latest_linux_x86_64/bin/salmon index -t ../transcriptome/Homo_sapiens.GRCh38.cdna.all.fa.gz hsa_index
