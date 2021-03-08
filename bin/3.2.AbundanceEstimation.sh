0fdeae0960e60bc44cbb2ee2168dc46a9e13f879
Rodolfo Chavez <rchavez@nodo7.conabio.gob.mx>
2021-03-07 22:04:56 -0600

    Update to perform transcript quantification in all samples

--- bin/3.2.AbundanceEstimation.sh
+++ bin/3.2.AbundanceEstimation.sh
@@ -1,10 +1,10 @@
 ##This script performs pseudoalignment and abundance estimation of transcripts using salmon
 mkdir -p ../salmon_quants
-for i in {1..6};  do
+for i in {1..12};  do
 r1="s${i}_R1.fastq.gz"
 r2="s${i}_R2.fastq.gz"
 printf "\n"
 echo "Processing sample s$i"
 ##Use salmon to quantificate transcripts
-~/salmon-latest_linux_x86_64/bin/salmon quant -i ../transcriptome/hsa_index -l A -1 ../data/paired/$r1 -2 ../data/paired/$r2 -p 8 --validateMappings -o ../salmon_quants/s${i}_quant
+~/salmon-latest_linux_x86_64/bin/salmon quant -i ../transcriptome/hsa_index -l A -1 ../data/$r1 -2 ../data/$r2 -p 8 --validateMappings -o ../salmon_quants/s${i}_quant
 done
