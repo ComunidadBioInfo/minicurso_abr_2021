6cd8f778b83f4b96278853f22ed7f66bf2cd4cc8
necrosnake91 <rodolfo.peacewalker@gmail.com>
2021-04-11 19:22:54 -0500

    Slides proposal

--- bin/4.DE_analysis.R
+++ bin/4.DE_analysis.R
@@ -1,24 +1,36 @@
 library(tximeta)
-library(tximportData)
+library(tidyverse)
 library(SummarizedExperiment)
 library(DESeq2)
+library(PCAtools)
+library(pheatmap)
+source("../bin/functions.R")
 
+coldata <- read.delim(here::here("output/salmon_quants/metadata.txt"))
+coldata <- coldata %>% dplyr::rename(names = unique_id)
+rownames(coldata) <- coldata$names
+dir <- file.path(here::here("output/salmon_quants"))
+list.files(dir) 
+coldata$files <- file.path(dir,paste0(coldata$Sample,"_quant"),"quant.sf")
+data.frame(coldata$Sample, file.exists(coldata$files))
 
-dir <- ("../salmon_quants/")
-samples <- read.table(file.path(dir,  "metadata.txt"), he = T)
-files <- file.path(dir, paste0(samples$Sample, "_quant"), "quant.sf")
-data.frame(file = files,  exist = file.exists(files))
-
-coldata <- data.frame(files,  names = samples$Unique_id, condition = samples$Condition, stringsAsFactors = F)
 se <- tximeta(coldata)
-colData(se)
-gse <- summarizeToGene(se)
-assayNames(se)
-rowRanges(se)
-seqinfo(se)
-edb <- retrieveDb(se)
-class(edb)
-gse <- summarizeToGene(se)
-rowRanges(gse)
+se$Condition <- factor(se$Treatment)
+table(se$Condition)
+dds <- DESeqDataSet(se, design = ~ Condition)
+keep <- rowSums(counts(dds) >= 3) >= 6
+dds <- dds[keep, ]
+dds <- DESeq(dds)
 
-rowRanges(gse)
+rld <- rlog(dds, blind = F)
+PCA <- pca(assay(rld), scale = T, metadata = coldata) 
+
+res <- results(dds)
+res_lfc <- lfcShrink(dds, coef = "Condition_Verafinib_vs_Control",  type = "apeglm")
+group <- coldata$Treatment
+dds$group <- group
+DEG <- as.data.frame(res)
+significant <- DEG %>% filter(log2FoldChange > 0 & padj < 0.1 |
+                                log2FoldChange < 0 & padj < 0.1)
+norm_counts <- counts(dds, normalized = T)
+annotation_col <- coldata[, c(2, 3)]
